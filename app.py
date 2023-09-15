fb24ad896ea204892f1a8a1d7bf9027172902b5e
mahouboy23 <mahoudjop@gmail.com>
2023-09-15 08:50:50 +0000

    Your commit message

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='Templates', static_folder='Static')
 
 @app.route('/')
-def home():
-    return "Hello, World!"
-
-@app.route('/Welcome')
 def index():
     return render_template('index.html')
 
